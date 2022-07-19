c22548d098bf2820d7713e54e1481c3cb64c36f1
bahar <baharkh127@gmail.com>
2022-07-19 13:51:26 +0430

    added request entity

--- Entities.py
+++ Entities.py
@@ -11,9 +11,27 @@ class ServiceType(Enum):
     web_gate = "WEB_GATE"
 
 
+class RequestType(Enum):
+    mobile_order = "MOBILE_ORDER"
+    web_order = "WEB_ORDER"
+    message_delivery = "MESSAGE_DELIVERY"
+    view_info_mobile = "VIEW_INFO_MOBILE"
+    view_info_web = "VIEW_INFO_WEB"
+    delivery_request = "DELIVERY_REQUEST"
+    followup_order = "FOLLOWUP_ORDER"
+
+
 class Service:
-    def __init__(self, service_type: ServiceType, number, mean_service_time, max_time):
+    def __init__(self, service_type: ServiceType, number, mean_service_time, max_time, error_rate):
         self.service_type = service_type
         self.number = number
         self.mean_time = mean_service_time
         self.max_time = max_time
+        self.error_rate = error_rate
+
+
+class Request:
+    def __init__(self, request_type, occurrence_likelihood, services):
+        self.request_type = request_type
+        self.occurrence_likelihood = occurrence_likelihood
+        self.services = services
--- main.py
+++ main.py
@@ -1,8 +1,45 @@
 import simpy
-from Entities import ServiceType, Service
+from Entities import ServiceType, Service, RequestType, Request
 
 
-def get_inputs():
+def init_requests():
+    requests = {}
+    likelihoods = [0.2, 0.1, 0.05, 0.25, 0.15, 0.2, 0.05]
+
+    services = {
+        RequestType.mobile_order: [ServiceType.api_gate, ServiceType.orders_management, ServiceType.payment],
+        RequestType.web_order: [ServiceType.web_gate, ServiceType.orders_management, ServiceType.payment],
+        RequestType.message_delivery: [ServiceType.api_gate, ServiceType.customer_management,
+                                       ServiceType.delivery_communication],
+        RequestType.view_info_mobile: [ServiceType.api_gate, ServiceType.restaurant_management],
+        RequestType.view_info_web: [ServiceType.web_gate, ServiceType.restaurant_management],
+        RequestType.delivery_request: [ServiceType.web_gate, ServiceType.restaurant_management,
+                                       ServiceType.delivery_communication],
+        RequestType.followup_order: [ServiceType.api_gate, ServiceType.orders_management]
+    }
+
+    request_orders = [
+        RequestType.mobile_order,
+        RequestType.web_order,
+        RequestType.message_delivery,
+        RequestType.view_info_mobile,
+        RequestType.view_info_web,
+        RequestType.delivery_request,
+        RequestType.followup_order
+    ]
+
+    for request_type, l in zip(request_orders, likelihoods):
+        requests[request_type] = Request(request_type, l, services[request_type])
+
+    return requests
+
+
+def init_services(service_numbers):
+    services = {}
+
+    mean_service_times = [8, 5, 6, 9, 12, 2, 3]
+    error_rates = [0.02, 0.02, 0.03, 0, 1, 0, 2, 0.01, 0.01]
+
     service_orders = [
         ServiceType.restaurant_management,
         ServiceType.customer_management,
@@ -13,18 +50,23 @@ def get_inputs():
         ServiceType.web_gate,
     ]
 
-    mean_service_times = [8, 5, 6, 9, 12, 2, 3]
-
-    services = {}
-
-    service_numbers = [int(i) for i in input().split()]
-
-    requests_rate = int(input())
-    time = int(input())
     max_times = [int(i) for i in input().split()]
 
     for i, (service_type, service_num) in zip(service_orders, service_numbers):
-        services[service_type.value] = Service(service_type, service_num, mean_service_times[i], max_times[i])
+        services[service_type.value] = Service(service_type, service_num, mean_service_times[i], max_times[i],
+                                               error_rates[i])
+
+    return services
+
+
+def get_inputs():
+    service_numbers = [int(i) for i in input().split()]
+
+    request_rate = int(input())
+    time = int(input())
+
+    services = init_services(service_numbers)
+    requests = init_requests()
 
 
 def simulation():
